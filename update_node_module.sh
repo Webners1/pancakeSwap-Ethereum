8d967c3cb8ffb1f2930fe284810c703e37122b5a
Webners1 <Webners1>
2022-03-05 23:32:45 +0500

    add

--- update_node_module.sh
+++ update_node_module.sh
@@ -1,5 +1,5 @@
 echo "Updating Node Modules"
-cp -r packages/pancakeswap node_modules/@pancakeswap
+cp -r packages/ node_modules/@pancakeswap
 echo "node_modules updates"
 echo "node_modules updates"
 echo "node_modules updates"
