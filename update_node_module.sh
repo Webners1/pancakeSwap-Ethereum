49b2f6d982c7b046271757664fe88c6e6bd6508f
Webners1 <Webners1>
2022-03-05 23:55:34 +0500

    add

--- update_node_module.sh
+++ update_node_module.sh
@@ -1,5 +1,7 @@
 echo "Updating Node Modules"
-cp -r packages/ node_modules/@pancakeswap
+cp -r packages/uikit node_modules/@pancakeswap
+cp -r packages/sdk node_modules/@pancakeswap
+cp -r packages/eslint-config-pancake node_modules/@pancakeswap
 echo "node_modules updates"
 echo "node_modules updates"
 echo "node_modules updates"
