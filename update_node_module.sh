57eece09142965da0a1566e62ed33436ac4eb445
Webners1 <Webners1>
2022-03-06 00:12:06 +0500

    ad

--- update_node_module.sh
+++ update_node_module.sh
@@ -1,7 +1,7 @@
 echo "Updating Node Modules"
-cp -r packages/uikit node_modules/@pancakeswap
-cp -r packages/sdk node_modules/@pancakeswap
-cp -r packages/eslint-config-pancake node_modules/@pancakeswap
+cp -r packages/uikit node_modules/@pancakeswap/
+cp -r packages/sdk node_modules/@pancakeswap/
+cp -r packages/eslint-config-pancake node_modules/@pancakeswap/
 echo "node_modules updates"
 echo "node_modules updates"
 echo "node_modules updates"
@@ -9,10 +9,15 @@ echo "node_modules updates"
 echo "node_modules updates"
 echo "node_modules updates"
 echo "node_modules updates"
-target="node_modules/@pancakeswap"
+target="node_modules"
 let count=0
 for f in "$target"/*
 do
+       if [ $(basename $f) == "@pancakeswap" ]
+       then 
+       for j in "$target"/*
+              do
+              echo $(basename $j)
     echo $(basename $f)
     let count=count+1
 done
